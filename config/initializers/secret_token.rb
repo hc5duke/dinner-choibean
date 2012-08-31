445420cf4d18a95198113b48c6581354fde78dc2
hc5duke <hc5duke@gmail.com>
2012-08-31 10:38:24 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinner::Application.config.secret_token = '8cafb8bd932dd51dc328b53f412978d9ddfa9d25ce8859d8a4e742a97e31af69b9db38d63d01765191d188d796ab1c257f318ee04ab06db254ea55db62c1a168'
