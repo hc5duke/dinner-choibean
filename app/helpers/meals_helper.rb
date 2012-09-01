55b0df8bbb6d1ec879f0e52459d58d6fdb0f9f8a
hc5duke <hc5duke@gmail.com>
2012-08-31 19:34:22 -0700

    ok

--- app/helpers/meals_helper.rb
+++ app/helpers/meals_helper.rb
@@ -2,6 +2,6 @@ module MealsHelper
   DAY_MARKERS = %w(M T W H F S N)
 
   def nice_print(date)
-    "#{date.strftime '%m-%d'} #{DAY_MARKERS[date.days_to_week_start]}"
+    "#{date.strftime '%01m/%-1d'} #{DAY_MARKERS[date.days_to_week_start]}"
   end
 end
